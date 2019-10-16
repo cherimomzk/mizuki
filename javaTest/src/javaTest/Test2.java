20e49b7d2e5257b4c616bd9a23420a81dbfbe49f
瑞 <@mizuki.mshome.net>
2019-10-16 20:44:48 +0900

    コミットテスト

---
+++ javaTest/src/javaTest/Test2.java
@@ -0,0 +1,19 @@
+package javaTest;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+	int a = 1;
+	int b = 2;
+	int c = a+b;
+
+	System.out.println(c);
+
+	c=c+1;
+
+	System.out.println(c);
+
+
+}
+}
