52ab74bce5d1b66af9d236412e23492fe434c5cd
瑞 <@mizuki.mshome.net>
2019-11-05 19:06:00 +0900

    付録問題8

---
+++ javaTest/src/javaTest/Mondai8.java
@@ -0,0 +1,17 @@
+package javaTest;
+
+public class Mondai8 {
+
+	public static void main(String[] args) {
+
+
+		Mondai8Sub mondai8sub = new Mondai8Sub();
+		int a = 0;
+		for (int i = 1; i <= 3; i++) {
+
+			a = mondai8sub.keisanA(a);
+
+		}
+
+	}
+}
---
+++ javaTest/src/javaTest/Mondai8Sub.java
@@ -0,0 +1,18 @@
+package javaTest;
+
+public class Mondai8Sub {
+
+	public void keisanA(String a) {
+		
+	}
+
+
+		public int keisanA(int a) {
+
+		System.out.println("aの値は"+a+"です。");
+		a = a+1;
+		return a ;
+
+
+	}
+}
