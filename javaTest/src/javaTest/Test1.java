37ad273151195d53a273e37fc05b426d545a3a6f
瑞 <@mizuki.mshome.net>
2019-11-05 19:02:04 +0900

    付録問題8

--- javaTest/src/javaTest/Test1.java
+++ javaTest/src/javaTest/Test1.java
@@ -1,9 +1,31 @@
 package javaTest;
 
+import java.util.Scanner;
+
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("年齢を入力してください");
+
+		int nenrei = 0;	//変数nenreiを作成
+		int kakaku = 0;	//変数kakakuを作成
+
+		nenrei = sc.nextInt();
+
+		if (nenrei == 0) {	//年齢が0歳だったら…
+			kakaku = 100;
+
+		} else {	//年齢が0歳ではなかったら…
+			kakaku = 500;
+
+		}
+
+		System.out.println(kakaku);
+
+		sc.close();
 
 	}
 
