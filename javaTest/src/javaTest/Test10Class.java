7790fa7d05a3dff031ba7d9b024d4618f6d28e76
瑞 <@mizuki.mshome.net>
2019-11-26 21:21:59 +0900

    付録問題10

---
+++ javaTest/src/javaTest/Test10Class.java
@@ -0,0 +1,43 @@
+package javaTest;
+
+import java.util.Scanner;
+
+public class Test10Class {
+
+	public static void main(String[] args) {
+
+		System.out.println("モードを選択してください。1 or 2");
+
+		Scanner str = new Scanner(System.in);
+
+		int mode = str.nextInt();
+
+		System.out.println("生まれた年を西暦で入力してください。");
+
+		Class.umare = str.nextInt();
+
+		while (mode != 1) {
+			if (mode != 2) {
+
+				System.out.println("モードを選択してください。1 or 2");
+
+				mode = str.nextInt();
+
+			} else {
+				break;
+			}
+		}
+
+		if (mode == 1) {
+			Sub sub = new Sub();
+			Sub.calcage();
+
+		} else {
+			Sub sub = new Sub();
+			Sub.calcyear();
+
+			str.close();
+		}
+
+	}
+}
